6deea82a37a071b1330ff9cd081148e3d884e914
Jahnavi Kharva <71918361+JahnaviKharva@users.noreply.github.com>
2024-10-19 23:34:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,22 @@
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
+
+# Load the environment variables from the .env file
+load_dotenv()
 
 class Config:
     """Base configuration with default settings."""
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'your_jwt_secret_key')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)  # Set to 1 hour
 
     # Database configuration
-    DB_HOST = os.environ.get('DB_HOST', 'localhost')
-    DB_NAME = os.environ.get('DB_NAME', 'purchase_db')
-    DB_USER = os.environ.get('DB_USER', 'postgres')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD', 'Janu@123')
-    DB_PORT = os.environ.get('DB_PORT', 5432)
+    DB_HOST = os.getenv('DATABASE_HOST')  # No fallback needed
+    DB_NAME = os.getenv('DATABASE_NAME')
+    DB_USER = os.getenv('DATABASE_USER')
+    DB_PASSWORD = os.getenv('DATABASE_PASSWORD')
+    DB_PORT = int(os.getenv('DATABASE_PORT'))
 
 
 class DevelopmentConfig(Config):
