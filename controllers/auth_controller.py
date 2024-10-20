ae1392396f683f39ff488586992d271002d9b971
Jahnavi Kharva <71918361+JahnaviKharva@users.noreply.github.com>
2024-10-20 10:28:02 +0530

    Update auth_controller.py

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, unset_jwt_cookies
 from models.database import get_db_connection
 
 auth_bp = Blueprint('auth_bp', __name__)
